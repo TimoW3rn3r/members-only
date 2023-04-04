ddeec5ad3a5e3523a58dd10e9cf4ff741dd933c2
Werner <turbotimoh@gmail.com>
2023-04-04 09:27:59 +0545

    Add new and create methods to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
